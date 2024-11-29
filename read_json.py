3b8ed121dca8f81bb962569018bdca294ab7f9aa
sabrinasilva26 <sabrinasilva.art@gmail.com>
2024-11-29 22:20:45 +0000

    added read_json and data.json

---
+++ read_json.py
@@ -0,0 +1,17 @@
+import json 
+
+try:
+    path=input()
+    file= open(path, "rt",encoding='utf-8')
+    json_data = file.read()
+    data=json.loads(json_data)
+    print(data)
+    file.close()  
+except:
+    print("Ocorreu um erro!")
+finally:
+    print("Processo concluído!")
+    
+
+
+ 
